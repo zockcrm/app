aa79ae9089e34b5c57e4ad3d35d02fcdfbde9f9f
a.kucherenko <a.kucherenko@powwwer.io>
2021-01-03 18:41:37 +0700

    Initial sources

---
+++ api-platform-2.5.7/api/src/Entity/ApiUser.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource
+ * @ORM\Entity
+ */
+class ApiUser
+{
+    /**
+     * @var int The entity Id
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string A nice person
+     *
+     * @ORM\Column
+     * @Assert\NotBlank
+     */
+    public $name = '';
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
